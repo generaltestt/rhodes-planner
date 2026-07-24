9935c64871bfee9fa7f524b2f75ce022491bb97b
Sava Rabinskis <srabinskis@192.168.1.146>
2026-07-24 16:38:01 +0100

    Bind to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -499,4 +499,4 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => console.log(`Rhodes Planner running on http://localhost:${PORT}`));
+server.listen(PORT, "0.0.0.0", () => console.log(`Rhodes Planner running on http://0.0.0.0:${PORT}`));
